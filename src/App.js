4e7b4210a16bd437bbe0f970ef6206ca2aa08f38
MasalingChristo <s11710064@student.unklab.ac.id>
2021-05-11 10:54:22 -1200

    menambahkan form login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () =>{
+
+    if(!localStorage.getItem('username')) return <LoginForm />
     return(
 
         <ChatEngine
             height="100vh"
             projectID="90ae6b1b-0545-45bf-9ac8-b7b528144def"
-            userName="javascriptmastery"
-            userSecret="123123"
-            renderChatFeed={(chatAppProps)=><ChatFeed {... chatAppProps} />}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps)=> <ChatFeed {... chatAppProps} />}
 
         />
 
